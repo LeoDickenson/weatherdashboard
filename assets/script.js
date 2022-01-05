c28378e6943060e6ee652aefbbc6a9ca0d7925f2
Leo Dickens <dickens.leo@gmail.com>
2022-01-04 22:00:58 -0600

    added functionality to display today's weather for submitted city

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 var userInput = document.getElementById("userInput")
 var submitBtn = document.getElementById("submitBtn")
+
 var historyOne = document.getElementById("hs1")
 var historyTwo = document.getElementById("hs2")
 var historyThree = document.getElementById("hs3")
+
 var resetFormVar = document.getElementById("resetForm")
+
+var todayTemp = document.getElementById('todayTemp')
+var todayWind = document.getElementById('todayWind')
+var todayHumid = document.getElementById('todayHumid')
 
 historyOne.textContent = localStorage.getItem("histSlot1")
 historyTwo.textContent = localStorage.getItem("histSlot2")
@@ -11,13 +17,18 @@ historyThree.textContent = localStorage.getItem("histSlot3")
 
 function fetchWeather(a) {fetch("https://api.openweathermap.org/data/2.5/weather?q=" +a+ "&appid=e4077867c523709fde20e7291ca0cdf8").then(function (response) {
     return response.json();}).then(function (data) {
-        let todayTemp = data.main.temp
-        let todayWind = data.main.wind
-        let todayHumid = data.main.humidity
+        let todayTempVar = 'Temperature: ' + data.main.temp + ' Degrees Kelvin'
+        let todayWindVar = 'Wind: ' + data.wind.speed + 'mph'
+        let todayHumidVar = 'Humidity: ' + data.main.humidity + '%'
+        
+        todayTemp.textContent = todayTempVar
+        todayWind.textContent = todayWindVar
+        todayHumid.textContent = todayHumidVar
+
         console.log(data)
-        console.log(todayTemp)
-        console.log(todayWind)
-        console.log(todayHumid)
+        console.log(todayTempVar)
+        console.log(todayWindVar)
+        console.log(todayHumidVar)
     });}
     
     function addHistory() {
