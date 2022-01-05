553289eccab8103a02dd7936dfa82a7a8bc60961
Leo Dickens <dickens.leo@gmail.com>
2022-01-04 21:42:50 -0600

    cleaned up html, staging for weather results

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,38 @@
-// e4077867c523709fde20e7291ca0cdf8
 var userInput = document.getElementById("userInput")
 var submitBtn = document.getElementById("submitBtn")
 var historyOne = document.getElementById("hs1")
 var historyTwo = document.getElementById("hs2")
 var historyThree = document.getElementById("hs3")
-var userHistory = document.getElementsByClassName(".history")
 var resetFormVar = document.getElementById("resetForm")
 
+historyOne.textContent = localStorage.getItem("histSlot1")
+historyTwo.textContent = localStorage.getItem("histSlot2")
+historyThree.textContent = localStorage.getItem("histSlot3")
 
-function fetchWeather(a) {fetch("https://api.openweathermap.org/data/2.5/weather?q=" +a+ "&appid=e4077867c523709fde20e7291ca0cdf8")
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-});}
-
-function addHistory(a) {
-    historyOne.textContent = a
+function fetchWeather(a) {fetch("https://api.openweathermap.org/data/2.5/weather?q=" +a+ "&appid=e4077867c523709fde20e7291ca0cdf8").then(function (response) {
+    return response.json();}).then(function (data) {
+        let todayTemp = data.main.temp
+        let todayWind = data.main.wind
+        let todayHumid = data.main.humidity
+        console.log(data)
+        console.log(todayTemp)
+        console.log(todayWind)
+        console.log(todayHumid)
+    });}
+    
+    function addHistory() {
+        historyThree.textContent = historyTwo.textContent
+        historyTwo.textContent = historyOne.textContent
+        historyOne.textContent = userInput.value
+        localStorage.setItem('histSlot1',historyOne.textContent)
+        localStorage.setItem('histSlot2',historyTwo.textContent)
+        localStorage.setItem('histSlot3',historyThree.textContent)
+        return
 }
 
 submitBtn.addEventListener("click", function(e){
     e.preventDefault();
     fetchWeather(userInput.value);
-    addHistory(userInput.value);
+    addHistory();
     resetFormVar.reset()
-    console.log(historyOne)
 })
